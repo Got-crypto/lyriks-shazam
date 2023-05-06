bb828c6d44bbb907253845b7779945dedb9bf8ac
Got-crypto <pecpalmplay@gmail.com>
2023-05-06 04:15:06 +0200

    done making  a sidebar for mobile view

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import { RiCloseLine } from 'react-icons/ri'
 
 import logo from '../assets/logo.png'
 import { links } from '../assets/constants'
+import { HiOutlineMenu } from 'react-icons/hi'
 
 const NavLinks = ({handleClick}) => {
     return <div className='mt-10 '>
@@ -27,9 +28,25 @@ export default function Sidebar(){
     console.log('process.env.REACT_PUBLIC_X_RAPIDAPI_KEY', process.env.REACT_APP_X_RAPIDAPI_KEY)
 
     return (
-        <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
-            <img src={logo} alt="logo" className='w-full h-14 object-contain' />
-            <NavLinks />
-        </div>
+        <>
+            <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
+                <img src={logo} alt="logo" className='w-full h-14 object-contain' />
+                <NavLinks />
+            </div>
+
+            <div className='absolute md:hidden block top-6 right-3'>
+                {
+                    mobileMenuOpen ? (
+                        <RiCloseLine onClick={()=>setMobileMenuOpen(!1)} className='w-6 h-6 text-white mr-2' />
+                    ) : (
+                        <HiOutlineMenu onClick={()=>setMobileMenuOpen(!0)} className='w-6 h-6 text-white mr-2' />
+                    )
+                }
+            </div>
+            <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+                <img src={logo} alt="logo" className='w-full h-14 object-contain' />
+                <NavLinks handleClick={()=>setMobileMenuOpen(false)} />
+            </div>
+        </>
     )
 }
