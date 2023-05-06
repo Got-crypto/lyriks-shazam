275393c11a5b25d524d0e2c4501912ac467bcd27
Got-crypto <pecpalmplay@gmail.com>
2023-05-06 02:40:58 +0200

    just created a desktop and tablet sidebar, beginning mobile sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { RiCloseLine } from 'react-icons/ri'
 
-import { logo } from '../assets'
-// import { loader } from '../assets'
-// import { links } from '../assets/constants'
+import logo from '../assets/logo.png'
+import { links } from '../assets/constants'
 
-const NavLinks = () => {
-    <div className='mt-10 '>
-        {[].map(item => (
-            <NavLink>
+const NavLinks = ({handleClick}) => {
+    return <div className='mt-10 '>
+        {links.map(item => (
+            <NavLink 
+                key={item.name}
+                to={item.to}
+                onClick={ ()=> handleClick && handleClick()}
+                className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400'
+            >
+                <item.icon className='w-6 h-6 mr-2' />   
                 {item.name}
             </NavLink>
         ))}
@@ -23,7 +28,7 @@ export default function Sidebar(){
 
     return (
         <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
-            <img  alt="logo" className='w-full h-14 object-contain' />
+            <img src={logo} alt="logo" className='w-full h-14 object-contain' />
             <NavLinks />
         </div>
     )
