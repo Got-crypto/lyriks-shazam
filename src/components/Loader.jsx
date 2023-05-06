3b93c275110abf7720a3f5dc7baf20d58e5a00f2
Got-crypto <pecpalmplay@gmail.com>
2023-05-06 18:31:44 +0200

    added top plays content and loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,12 @@
-export default function Loader(){
+import loader from '../assets/loader.png'
+
+export default function Loader({ title }){
     return (
-        <p>
-            Loader
-        </p>
+        <div className="w-full flex justify-center items-center flex-col">
+            <img src={loader} alt='loader' className="w-32 h-32 object-contain" />
+            <h1 className='font-bold text-2xl text-white mt-2'>
+                {title || 'Loading'}
+            </h1>
+        </div>
     )
 }
